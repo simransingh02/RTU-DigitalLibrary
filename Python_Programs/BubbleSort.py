b8f423eb2c039245086e2f8a3f7b54250c541d64
simransingh02 <73027627+simransingh02@users.noreply.github.com>
2020-10-23 00:08:05 +0530

    Update BubbleSort.py
    initializing n for size of arrays which reduces little complexity.

--- Python_Programs/BubbleSort.py
+++ Python_Programs/BubbleSort.py
@@ -1,12 +1,11 @@
 def bubbleSort(array):
-    sizeOfArray = len(array)
+    n = len(array)
 
-    # Traverse through all array elements
-    for i in range(sizeOfArray - 1):
+    for i in range(n - 1):
         # range(n) also work but outer loop will repeat one time more than needed.
 
         # Last i elements are already in place
-        for j in range(0, sizeOfArray - i - 1):
+        for j in range(0, n- i - 1):
 
             # traverse the array from 0 to sizeOfArray-index-1
             # Swap if the element found is greater
@@ -15,8 +14,7 @@ def bubbleSort(array):
                 array[j], array[j + 1] = array[j + 1], array[j]
 
 
-# Driver code to test above
-print("Enter the Array:")
+print("Enter Array:")
 arr = [int(x) for x in input().split()]
 bubbleSort(arr)
 
