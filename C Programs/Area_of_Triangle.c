a73b0c99925fc7d2782a33ccd8cfe5beaf011e16
simransingh02 <73027627+simransingh02@users.noreply.github.com>
2020-10-31 23:56:24 +0530

    Update Area_of_Triangle.c

--- C Programs/Area_of_Triangle.c
+++ C Programs/Area_of_Triangle.c
@@ -1,12 +1,15 @@
 //C program to find the area of a triangle
 
 #include<stdio.h>
+#include<conio.h>
 void main()
-{ float a,b,h;
-printf("Enter the base : ");
+{ 
+float a,b,h;
+printf("Enter the base of traingle : ");
 scanf("%f",&b);
-printf("Enter the height : ");
+printf("Enter the height of the traingle: ");
 scanf("%f",&h);
 a=0.5*b*h;
-printf("Area is %f",a);
+printf("Area of traingle is %f",a);
+getch();
 }
